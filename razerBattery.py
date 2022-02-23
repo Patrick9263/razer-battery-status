39bdbbc7dfb11f44b55a082a3edb9224d8279c0c
patrick9263 <psmith9263@gmail.com>
2022-02-23 01:02:18 -0500

    Minor edits

--- razerBattery.py
+++ razerBattery.py
@@ -36,39 +36,47 @@ def getBatteryStats(test=None):
         if "Razer Viper Ultimate (Wireless)" == device.name:
             viper = device
 
-    if None == viper:
-        print("n/a")
-
     isCharging = False
-    if viper.is_charging:
-        isCharging = True
-    if viper.is_charging and viper.battery_level > 26:
-        status.setIsWarned(False)
+    if viper == None:
+        print("n/a")
+        return (False, 0)
+    else:
+        if viper.is_charging:
+            isCharging = True
+        if viper.is_charging and viper.battery_level > 26:
+            status.setIsWarned(False)
 
-    return (isCharging, viper.battery_level)
+        return (isCharging, viper.battery_level)
 
+
+def note(_):
+    os.system("gedit $HOME/Documents/notes.txt")
 
 
 def sendNotification(isCharging, percentage):
-    if not isCharging and percentage < 25 and not status.getIsWarned():
-        notification.notify(
-            #title of the notification,
-            title = "Razer Mouse Low Battery",
-            #the body of the notification
-            message = "{}%".format(percentage),
-            #creating icon for the notification
-            #we need to download a icon of ico file format
-            app_icon = r"/home/patrick/Pictures/razer-logo.png",
-            # the notification stays for 10sec
-            timeout  = 10
-        )
-        status.setIsWarned(True)
+    if not isCharging and percentage > 0 and percentage < 25 and not status.getIsWarned():
+        try:
+            notification.notify(
+                #title of the notification,
+                title = "Razer Mouse Low Battery",
+                #the body of the notification
+                message = "{}%".format(percentage),
+                #creating icon for the notification
+                #we need to download a icon of ico file format
+                app_icon = r"/home/patrick/Pictures/razer-logo.png",
+                # the notification stays for 10sec
+                timeout  = 10
+            )
+            status.setIsWarned(True)
+        except:
+            print('Error while sending Low Battery Notification!')
 
 async def refreshBatteryStatus(razer_command):
     while status.getIsRunning():
         (isCharging, percentage) = getBatteryStats()
         chargeText = "Charging" if isCharging else "Not Charging"
         new_label = "Razer Mouse: {} {}%".format(chargeText, str(percentage))
+        # print(new_label)
         razer_command.set_label(new_label)
         sendNotification(isCharging, percentage)
         await asyncio.sleep(5)
@@ -80,6 +88,11 @@ def quit(_):
 
 def menu():
     menu = gtk.Menu()
+
+    # command_one = gtk.MenuItem()
+    # command_one.set_label('My Notes')
+    # command_one.connect('activate', note)
+    # menu.append(command_one)
 
     razer_command = gtk.MenuItem()
     razer_command.set_label(str(getBatteryStats()[1]))
@@ -99,21 +112,26 @@ def runGtk():
 async def main():
     print('Running Razer Tray...')
 
-    indicator = appindicator.Indicator.new(
-        "myrazertray",
-        r"/home/patrick/Pictures/razer-logo.png",
-        appindicator.IndicatorCategory.APPLICATION_STATUS
-    )
-    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    try:
+        indicator = appindicator.Indicator.new(
+            "myrazertray",
+            r"/home/patrick/Pictures/razer-logo.png",
+            appindicator.IndicatorCategory.APPLICATION_STATUS
+        )
+        indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 
-    (myMenu, razer_command) = menu()
-    indicator.set_menu(myMenu)
+        (myMenu, razer_command) = menu()
+        indicator.set_menu(myMenu)
 
-    x = threading.Thread(target=runGtk)
-    x.start()
-    await refreshBatteryStatus(razer_command)
+        x = threading.Thread(target=runGtk)
+        x.start()
+        await refreshBatteryStatus(razer_command)
 
-    print('Closing Razer Tray...')
+        print('Closing Razer Tray...')
+
+    except Exception as e:
+        print("Error while running Razer Tray:")
+        print(e)
 
 
 if __name__ == '__main__':
